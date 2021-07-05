6df880b11220ccbfc51eb6d2089400480f7c5a1c
berrybab6 <bedatuassefa@gmail.com>
2021-07-05 21:09:33 +0300

    create and fetch food

--- restaurant_api/api/schema.py
+++ restaurant_api/api/schema.py
@@ -1,3 +1,5 @@
+# from typing_extensions import Required
+from django.db.models import query
 import graphene
 
 from graphene_django import DjangoObjectType, DjangoListField
@@ -17,4 +19,26 @@ class Query(graphene.ObjectType):
         return Food.objects.all()
     def resolve_food(self, info, food_id):
         return Food.objects.get(id=food_id)
-    
+
+class FoodInput(graphene.InputObjectType):
+    id = graphene.Int()
+    name = graphene.String()
+    description = graphene.String()
+    type = graphene.String()
+    price = graphene.Float()
+    fastingFood = graphene.Boolean()
+
+class CreateFood(graphene.Mutation):
+    class Arguments:
+        food_data = FoodInput(required=True)
+    food = graphene.Field(FoodType)
+    @staticmethod
+    def mutate(root, info, food_data=None):
+        food_inistance = Food(name=food_data.name, description=food_data.description, type=food_data.type, price=food_data.price, fastingFood = food_data.fastingFood)
+        food_inistance.save()
+        return CreateFood(food=food_inistance)
+
+class Mutation(graphene.ObjectType):
+    create_food = CreateFood.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)

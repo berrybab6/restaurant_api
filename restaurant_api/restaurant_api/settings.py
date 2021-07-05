02e957472357d6e967affbb09d4d10c645963c23
berrybab6 <bedatuassefa@gmail.com>
2021-07-05 15:08:20 +0300

    api feature created

--- restaurant_api/restaurant_api/settings.py
+++ restaurant_api/restaurant_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {   
+    "SCHEMA": "api.schema.schema"
+}
